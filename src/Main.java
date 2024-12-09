323150671b961d7a50ff6a2429bc0273f640b75c
Claire <55174253+clairebearz32bit@users.noreply.github.com>
2024-12-09 05:14:53 -0800

    Merged branches.

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) throws NonASCIIException {
-        Tester tester = new Tester();
-//        tester.testAll();
-
-        RC4 cipher = new RC4("Attack at dawn", "Secret");
-        System.out.println(Cipher.Util.sToH(cipher.encrypt()));
-    }
-}
--- src/NonASCIIException.java
+++
@@ -1,4 +0,0 @@
-public class NonASCIIException extends Exception {
-    public NonASCIIException() {super();}
-    public NonASCIIException(String message) {super(message);}
-}
